5d600b01225599e13c2c889b5e2ee1cd3360d0f5
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-04-18 21:47:35 +0700

    change style file

--- src/components/common/ConnectWalletButton.jsx
+++ src/components/common/ConnectWalletButton.jsx
@@ -1,11 +1,13 @@
 import "./ConnectWalletButton.css";
 import React from "react";
 
-export default function ConnectWalletButton({ imgSrc, children, onClick }) {
+const ConnectWalletButton = ({ imgSrc, children, onClick }) => {
   return (
     <button className="connect-wallet-btn" onClick={onClick}>
       {imgSrc && <img className="btn-icon" src={imgSrc} alt="Connect Wallet" />}
       <span className="btn-label">{children}</span>
     </button>
   );
-}
+};
+
+export default ConnectWalletButton;
