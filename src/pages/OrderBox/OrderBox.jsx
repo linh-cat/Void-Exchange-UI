b072489eab79c034c1cd69f475d0192abe55c5b6
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-04-22 15:17:16 +0700

    FE: import select

--- src/pages/OrderBox/OrderBox.jsx
+++ src/pages/OrderBox/OrderBox.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import SelectToken from "../../components/common/SelectToken";
+import SelectCustom from "../../components/common/SelectCustom";
 import "./OrderBox.css";
 
 const OrderBox = ({ type }) => {
   return (
     <div className="order-box">
       <div className="test">
-        {/* <SelectToken
+        <SelectCustom
           options={[
-            { label: "BTC", value: "BTC", icon: BTC },
-            { label: "BNB", value: "BNB", icon: BNB },
-            { label: "ETH", value: "ETH", icon: ETH },
+            { label: "BTC", value: "BTC" },
+            { label: "BNB", value: "BNB" },
+            { label: "ETH", value: "ETH" },
           ]}
           defaultValue={"BTC"}
-        /> */}
+        />
       </div>
     </div>
   );
