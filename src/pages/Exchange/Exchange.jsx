63aadfea51f0a6a3a5130579f0f92cf141e116c2
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-04-24 12:16:52 +0700

    FE: set padding for exchange page

--- src/pages/Exchange/Exchange.jsx
+++ src/pages/Exchange/Exchange.jsx
@@ -4,7 +4,7 @@ import TabExchange from "../../components/TabExchange/TabExchange";
 
 const Exchange = () => {
   return (
-    <div className="exchange w-full h-full my-5">
+    <div className="exchange w-full h-full p-0 md:p-5">
       <div className="lg:grid lg:grid-cols-3 xl:grid-cols-5">
         <div className="lg:col-span-2 xl:col-span-4 left-side">01</div>
         <div className="right-side">
