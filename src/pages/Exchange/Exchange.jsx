74f18450faa2c8389c781c9a33dc328f7b65445d
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-04-24 12:03:31 +0700

    FE: change font all page

--- src/pages/Exchange/Exchange.jsx
+++ src/pages/Exchange/Exchange.jsx
@@ -4,14 +4,12 @@ import TabExchange from "../../components/TabExchange/TabExchange";
 
 const Exchange = () => {
   return (
-    <div>
-      <div className="exchange w-full h-full py-0 px-0 md:py-5 md:px-5">
-        <div className="lg:grid lg:grid-cols-3 xl:grid-cols-5">
-          <div className="lg:col-span-2 xl:col-span-4 left-side">01</div>
-          <div className="right-side">
-            <div className="w-full p-5 bg-card">
-              <TabExchange defaultValue="long" />
-            </div>
+    <div className="exchange w-full h-full my-5">
+      <div className="lg:grid lg:grid-cols-3 xl:grid-cols-5">
+        <div className="lg:col-span-2 xl:col-span-4 left-side">01</div>
+        <div className="right-side">
+          <div className="w-full p-5 bg-card">
+            <TabExchange defaultValue="long" />
           </div>
         </div>
       </div>
