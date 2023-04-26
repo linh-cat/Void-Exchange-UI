72e34e56cb650cdac5524695268f860ca9049120
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-04-26 15:06:25 +0700

    FE: change text size infor bar

--- src/pages/Exchange/ExchangeTopChart.jsx
+++ src/pages/Exchange/ExchangeTopChart.jsx
@@ -35,44 +35,44 @@ const ExchangeTopChart = () => {
       </div>
       <div className="group-infor p-1 flex flex-col sm:flex-row gap-1 sm:gap-1 md:gap-2 lg:gap-5">
         <div className="flex justify-between items-center sm:gap-1 md:gap-2 lg:gap-5 xl:gap-6 2xl:gap-7">
-          <div className="gap-1 font-very-small">
+          <div className="gap-1 font-very-small 2xl:text-base">
             <label className="text-slate-500">24h Change %</label>
             <div className="flex items-center gap-x-1">
               <img src={Up} alt="up" />
-              <span className="inline green-up font-very-small">0.34%</span>
+              <span className="inline green-up">0.34%</span>
             </div>
           </div>
-          <div className="font-very-small">
+          <div className="font-very-small 2xl:text-base">
             <label className="text-slate-500">24h Change</label>
-            <div className="green-up font-very-small">$163.72</div>
+            <div className="green-up">$163.72</div>
           </div>
-          <div className="font-very-small">
+          <div className="font-very-small 2xl:text-base">
             <label className=" text-slate-500">24h High</label>
             <div className="">$27,567.32 </div>
           </div>
-          <div className="font-very-small">
+          <div className="font-very-small 2xl:text-base">
             <label className=" text-slate-500">24h Low</label>
             <div className="">$27,033.61</div>
           </div>
         </div>
         <div className="flex justify-between items-center sm:gap-1 md:gap-2 lg:gap-5 xl:gap-6 2xl:gap-7">
-          <div className="font-very-small">
+          <div className="font-very-small 2xl:text-base">
             <label className="font-very-small text-slate-500">
               Volumn (BTC)
             </label>
             <div className="">55.003</div>
           </div>
-          <div className="font-very-small">
+          <div className="font-very-small 2xl:text-base">
             <label className=" text-slate-500">Volumn (USD)</label>
             <div className="">$1,505,660</div>
           </div>
           <div>
             <div className="flex">
-              <div className="font-very-small">
+              <div className="font-very-small 2xl:text-base">
                 <label className="text-slate-500">Next Fund Rate</label>
                 <div className="red-down">-0.00013%</div>
               </div>
-              <div className="font-very-small">
+              <div className="font-very-small 2xl:text-base">
                 <label className=" text-slate-500">/ Countdown</label>
                 <div className="">00:46:00</div>
               </div>
