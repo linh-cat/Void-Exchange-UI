ae62a7713b6611845861981944aeadad6159d579
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-04-27 21:59:44 +0700

    FE: setting chart

--- src/pages/Exchange/Exchange.jsx
+++ src/pages/Exchange/Exchange.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import "./Exchange.css";
 import TabExchange from "../../components/TabExchange/TabExchange";
 import ExchangeTopChart from "./ExchangeTopChart";
+import TradingViewChart from "./TradingViewChart";
 
 const Exchange = () => {
   return (
     <div className="exchange w-full h-full p-0 md:p-5">
-      <div className="md:mb-3">
+      <div className="">
         <ExchangeTopChart />
       </div>
       <div className="lg:grid lg:grid-cols-3 xl:grid-cols-5 gap-3">
-        <div className="lg:col-span-2 xl:col-span-4 left-side"></div>
+        <div className="lg:col-span-2 xl:col-span-4 left-side">
+          <TradingViewChart />
+        </div>
         <div className="right-side">
           <div className="w-full bg-card">
             <TabExchange defaultValue="long" />
