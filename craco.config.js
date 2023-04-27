ee1526e0ac58704927f17e2da6039e5834ddae3e
anhthii <nguyendanganhthi247@gmail.com>
2023-04-27 07:59:41 +0700

    Add cracao dev dependencies to alias import

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+const ImportAlias = require("craco-alias");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: ImportAlias,
+      options: {
+        source: "options",
+        baseUrl: "src", // this is from where all search in files will start
+        aliases: {
+          "@components": "./components", // path from src folder
+          "@icons": "./img/icons",
+          "@img": "./img",
+        },
+      },
+    },
+  ],
+};
