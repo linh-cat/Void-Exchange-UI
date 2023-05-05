2c7c69f79b9024e559f77f4388409d14ab367dff
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-05-05 21:34:15 +0700

    FE: add popup collateral

---
+++ src/components/common/CollateralPopup.jsx
@@ -0,0 +1,73 @@
+import React from "react"
+import "./CollateralPopup.css"
+
+const CollateralPopup = () => {
+  return (
+    <div className="collateral-popup text-xs p-3 font-medium">
+      <div className="collateral-header">
+        <label className="font-medium">
+          Change Collateral - <span className="red-down">Short ETH</span>
+        </label>
+        <div className="colateral-tab font-medium mt-3">
+          <button className="active">Add</button>
+          <button>Remove</button>
+        </div>
+      </div>
+      <div className="collateral-content mt-3">
+        {/* input */}
+        <div className="collateral-input p-3 flex flex-col gap-y-3">
+          <div className="top flex justify-between">
+            <label className="text-zinc-500">Add</label>
+            <div className="text-zinc-500">Max: 5.29</div>
+          </div>
+          <div className="middle flex justify-between">
+            <input placeholder="0.01" />
+            <div>USDT</div>
+          </div>
+          <div className="bottom flex justify-between">
+            <button className="px-3 py-1 bg-zinc-700 active">25%</button>
+            <button className="px-3 py-1 bg-zinc-700">50%</button>
+            <button className="px-3 py-1 bg-zinc-700">75%</button>
+            <button className="px-3 py-1 bg-zinc-700">100%</button>
+          </div>
+        </div>
+        {/* infor */}
+        <div className="collateral-infor mt-3 flex flex-col gap-y-2">
+          <div className="flex justify-between">
+            <label className="text-zinc-500">Position Size</label>
+            <label>
+              0.0261 <span className="text-zinc-500">($49.2)</span>
+            </label>
+          </div>
+          <div className="flex justify-between">
+            <label className="text-zinc-500">Collateral(USDT)</label>
+            <label>
+              4.96$ - <span className="text-zinc-500">$9.2</span>
+            </label>
+          </div>
+          <div className="flex justify-between">
+            <label className="text-zinc-500">Leverage</label>
+            <label>
+              10.0X - <span className="text-zinc-500">7.9X</span>
+            </label>
+          </div>
+          <div className="flex justify-between">
+            <label className="text-zinc-500">Index Price</label>
+            <label>$1.1119.0</label>
+          </div>
+          <div className="flex justify-between">
+            <label className="text-zinc-500">Liq.Price</label>
+            <label>
+              $2,079.0 - <span className="text-zinc-500">$ 2,123.2</span>
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="collateral-footer mt-3">
+        <button className="secondary-btn btn">Add Collateral</button>
+      </div>
+    </div>
+  )
+}
+
+export default CollateralPopup
--- src/pages/Exchange/ListPosition.jsx
+++ src/pages/Exchange/ListPosition.jsx
@@ -1,280 +1,78 @@
 import React from "react"
 import ETH from "@img/WETH.png"
+import Plus from "@img/icons/Plus.svg"
+import CollateralPopup from "@components/common/CollateralPopup"
 
 const ListPosition = () => {
   return (
-    <table className="w-full">
-      <thead className="border-b border-zinc-700 text-zinc-500 text-xs md:text-sm">
-        <tr>
-          <th scope="col" className="font-medium">
-            Market
-          </th>
-          <th scope="col" className="font-medium">
-            Size
-          </th>
-          <th scope="col" className="font-medium">
-            Net Value
-          </th>
-          <th scope="col" className=" font-medium">
-            Collateral
-          </th>
-          <th scope="col" className=" font-medium">
-            Entry Price
-          </th>
-          <th scope="col" className=" font-medium">
-            Index Price
-          </th>
-          <th scope="col" className=" font-medium">
-            PNL & ROE
-          </th>
-          <th scope="col" className=" font-medium">
-            Actions
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className="">
-          <th className="text-xs font-medium">
-            <div className="flex items-center gap-2">
-              <img src={ETH} alt="img" className="w-6 h-6" />
-              <div>
-                <label>ETH/USDT</label>
-                <div className="red-down">Short 10.0X</div>
+    <>
+      <CollateralPopup />
+      <table className="w-full">
+        <thead className="border-b border-zinc-700 text-zinc-500 text-xs md:text-sm">
+          <tr>
+            <th scope="col" className="font-medium">
+              Market
+            </th>
+            <th scope="col" className="font-medium">
+              Size
+            </th>
+            <th scope="col" className="font-medium">
+              Net Value
+            </th>
+            <th scope="col" className=" font-medium">
+              Collateral
+            </th>
+            <th scope="col" className=" font-medium">
+              Entry Price
+            </th>
+            <th scope="col" className=" font-medium">
+              Index Price
+            </th>
+            <th scope="col" className=" font-medium">
+              PNL & ROE
+            </th>
+            <th scope="col" className=" font-medium">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="">
+            <th className="text-xs font-medium">
+              <div className="flex items-center gap-2">
+                <img src={ETH} alt="img" className="w-6 h-6" />
+                <div>
+                  <label>ETH/USDT</label>
+                  <div className="red-down">Short 10.0X</div>
+                </div>
               </div>
-            </div>
-          </th>
-          <td className="text-xs font-medium">$49.9</td>
-          <td className="text-xs font-medium">$4.9</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label>1.96 USDT</label>
-              <label className="text-zinc-500">$1.96</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">$1,884.9</td>
-          <td className="text-xs font-medium">$2,001</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label className="green-up">+ $0.35</label>
-              <label className="text-zinc-500">+ 6,16</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">
-            <button className="secondary-btn py-2 px-2">CLOSE</button>
-          </td>
-        </tr>
-        <tr>
-          <th className="text-xs font-medium">
-            <div className="flex items-center gap-2">
-              <img src={ETH} alt="img" className="w-6 h-6" />
-              <div>
-                <label>ETH/USDT</label>
-                <div className="red-down">Short 10.0X</div>
+            </th>
+            <td className="text-xs font-medium">$49.9</td>
+            <td className="text-xs font-medium">$4.9</td>
+            <td className="text-xs font-medium">
+              <div className="flex flex-col gap-y-1">
+                <div className="flex gap-x-1 items-center">
+                  <label>1.96 USDT</label>
+                  <img src={Plus} alt="icon" className="w-5 cursor-pointer" />
+                </div>
+                <label className="text-zinc-500">$1.96</label>
               </div>
-            </div>
-          </th>
-          <td className="text-xs font-medium">$49.9</td>
-          <td className="text-xs font-medium">$4.9</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label>1.96 USDT</label>
-              <label className="text-zinc-500">$1.96</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">$1,884.9</td>
-          <td className="text-xs font-medium">$2,001</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label className="green-up">+ $0.35</label>
-              <label className="text-zinc-500">+ 6,16</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">
-            <button className="secondary-btn py-2 px-2">CLOSE</button>
-          </td>
-        </tr>
-        <tr>
-          <th className="text-xs font-medium">
-            <div className="flex items-center gap-2">
-              <img src={ETH} alt="img" className="w-6 h-6" />
-              <div>
-                <label>ETH/USDT</label>
-                <div className="red-down">Short 10.0X</div>
+            </td>
+            <td className="text-xs font-medium">$1,884.9</td>
+            <td className="text-xs font-medium">$2,001</td>
+            <td className="text-xs font-medium">
+              <div className="flex flex-col gap-y-1">
+                <label className="green-up">+ $0.35</label>
+                <label className="text-zinc-500">+ 6,16</label>
               </div>
-            </div>
-          </th>
-          <td className="text-xs font-medium">$49.9</td>
-          <td className="text-xs font-medium">$4.9</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label>1.96 USDT</label>
-              <label className="text-zinc-500">$1.96</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">$1,884.9</td>
-          <td className="text-xs font-medium">$2,001</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label className="green-up">+ $0.35</label>
-              <label className="text-zinc-500">+ 6,16</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">
-            <button className="secondary-btn py-2 px-2">CLOSE</button>
-          </td>
-        </tr>
-        <tr>
-          <th className="text-xs font-medium">
-            <div className="flex items-center gap-2">
-              <img src={ETH} alt="img" className="w-6 h-6" />
-              <div>
-                <label>ETH/USDT</label>
-                <div className="red-down">Short 10.0X</div>
-              </div>
-            </div>
-          </th>
-          <td className="text-xs font-medium">$49.9</td>
-          <td className="text-xs font-medium">$4.9</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label>1.96 USDT</label>
-              <label className="text-zinc-500">$1.96</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">$1,884.9</td>
-          <td className="text-xs font-medium">$2,001</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label className="green-up">+ $0.35</label>
-              <label className="text-zinc-500">+ 6,16</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">
-            <button className="secondary-btn py-2 px-2">CLOSE</button>
-          </td>
-        </tr>
-        <tr>
-          <th className="text-xs font-medium">
-            <div className="flex items-center gap-2">
-              <img src={ETH} alt="img" className="w-6 h-6" />
-              <div>
-                <label>ETH/USDT</label>
-                <div className="red-down">Short 10.0X</div>
-              </div>
-            </div>
-          </th>
-          <td className="text-xs font-medium">$49.9</td>
-          <td className="text-xs font-medium">$4.9</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label>1.96 USDT</label>
-              <label className="text-zinc-500">$1.96</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">$1,884.9</td>
-          <td className="text-xs font-medium">$2,001</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label className="green-up">+ $0.35</label>
-              <label className="text-zinc-500">+ 6,16</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">
-            <button className="secondary-btn py-2 px-2">CLOSE</button>
-          </td>
-        </tr>
-        <tr>
-          <th className="text-xs font-medium">
-            <div className="flex items-center gap-2">
-              <img src={ETH} alt="img" className="w-6 h-6" />
-              <div>
-                <label>ETH/USDT</label>
-                <div className="red-down">Short 10.0X</div>
-              </div>
-            </div>
-          </th>
-          <td className="text-xs font-medium">$49.9</td>
-          <td className="text-xs font-medium">$4.9</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label>1.96 USDT</label>
-              <label className="text-zinc-500">$1.96</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">$1,884.9</td>
-          <td className="text-xs font-medium">$2,001</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label className="green-up">+ $0.35</label>
-              <label className="text-zinc-500">+ 6,16</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">
-            <button className="secondary-btn py-2 px-2">CLOSE</button>
-          </td>
-        </tr>
-        <tr>
-          <th className="text-xs font-medium">
-            <div className="flex items-center gap-2">
-              <img src={ETH} alt="img" className="w-6 h-6" />
-              <div>
-                <label>ETH/USDT</label>
-                <div className="red-down">Short 10.0X</div>
-              </div>
-            </div>
-          </th>
-          <td className="text-xs font-medium">$49.9</td>
-          <td className="text-xs font-medium">$4.9</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label>1.96 USDT</label>
-              <label className="text-zinc-500">$1.96</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">$1,884.9</td>
-          <td className="text-xs font-medium">$2,001</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label className="green-up">+ $0.35</label>
-              <label className="text-zinc-500">+ 6,16</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">
-            <button className="secondary-btn py-2 px-2">CLOSE</button>
-          </td>
-        </tr>
-        <tr>
-          <th className="text-xs font-medium">
-            <div className="flex items-center gap-2">
-              <img src={ETH} alt="img" className="w-6 h-6" />
-              <div>
-                <label>ETH/USDT</label>
-                <div className="red-down">Short 10.0X</div>
-              </div>
-            </div>
-          </th>
-          <td className="text-xs font-medium">$49.9</td>
-          <td className="text-xs font-medium">$4.9</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label>1.96 USDT</label>
-              <label className="text-zinc-500">$1.96</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">$1,884.9</td>
-          <td className="text-xs font-medium">$2,001</td>
-          <td className="text-xs font-medium">
-            <div className="flex flex-col gap-y-1">
-              <label className="green-up">+ $0.35</label>
-              <label className="text-zinc-500">+ 6,16</label>
-            </div>
-          </td>
-          <td className="text-xs font-medium">
-            <button className="secondary-btn py-2 px-2">CLOSE</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+            </td>
+            <td className="text-xs font-medium">
+              <button className="secondary-btn py-2 px-2">CLOSE</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   )
 }
 
