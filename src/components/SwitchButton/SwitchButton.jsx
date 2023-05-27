870cef07b14aa412c467aa8111f4a1c1cd7a1172
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-05-27 14:19:22 +0700

    FE: change bg checked

--- src/components/SwitchButton/SwitchButton.jsx
+++ src/components/SwitchButton/SwitchButton.jsx
@@ -7,7 +7,7 @@ const SwitchButton = ({ label, className, onChange, value }) => {
         <input type="checkbox" className="sr-only peer" checked={value} readOnly />
         <div
           onClick={onChange}
-          className="w-11 h-6 border rounded-full peer peer-focus:ring-green-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-slate-500 after:border-slate-500 after:border after:rounded-full after:h-5 after:w-5 after:transition-all "
+          className="w-11 h-6 border rounded-full peer peer-focus:ring-green-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] peer-checked:after:bg-white after:bg-slate-500 after:border-slate-500 after:border after:rounded-full after:h-5 after:w-5 after:transition-all "
         ></div>
         {label && <span className="ml-2 text-sm">{label}</span>}
       </label>
