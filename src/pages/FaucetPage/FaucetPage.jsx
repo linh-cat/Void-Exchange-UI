784ff2127db1b4cb993f2d5a8f6d056743c9b56a
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-05-29 20:40:33 +0700

    FE: make faucet page and custom modal

--- src/pages/FaucetPage/FaucetPage.jsx
+++ src/pages/FaucetPage/FaucetPage.jsx
@@ -1,7 +1,122 @@
-import React from "react"
+import React, { useState } from "react"
+import ETH from "@img/WETH.png"
+import CardWrapper from "@components/CardWrapper/CardWrapper"
+import Mobo from "@img/morpho.png"
+import DAI from "@img/DAI.svg"
+import USDT from "@img/usdt.svg"
+import USDC from "@img/usdc.svg"
+import TableCustom from "@components/Table/TableCustom"
+import Button from "@components/Button/Button"
+import Modal from "@components/Modal/Modal"
+import { InputCustom } from "@components/common"
+import Metamask from "@img/metamask.png"
+
+const data = [
+  {
+    asset: "DAI",
+    token: DAI,
+    ballance: "10.00k"
+  },
+  {
+    asset: "USDT",
+    token: USDT,
+    ballance: "0"
+  },
+  {
+    asset: "USDC",
+    token: USDC,
+    ballance: "0"
+  }
+]
 
 const FaucetPage = () => {
-  return <div>FaucetPage</div>
+  const [openModal, setOpenModal] = useState(false)
+
+  const showModal = () => {
+    setOpenModal(true)
+  }
+
+  const columnDef = [
+    {
+      field: "asset",
+      headerName: "Asset",
+      headerClassName: "text-sm text-left",
+      classname: "text-left",
+      cellRenderer: (cell) => {
+        return (
+          <div className="flex items-center gap-2">
+            <img src={cell?.token} alt="token" className="h-10 w-10" />
+            <div className="flex flex-col gap-1">
+              <label>{cell?.asset}</label>
+              <span className="text-xs text-slate-500">{cell?.asset}</span>
+            </div>
+          </div>
+        )
+      }
+    },
+    {
+      field: "ballance",
+      headerName: "Wallet Balance",
+      headerClassName: "text-sm"
+    },
+    {
+      field: "action",
+      headerName: "",
+      cellRenderer: () => {
+        return <Button text="Faucet" type="secondary" className="py-2" isDefault={false} onClick={showModal} />
+      }
+    }
+  ]
+  return (
+    <div className="p-5 xl:p-0">
+      <Modal
+        open={openModal}
+        setOpen={setOpenModal}
+        header={
+          <div className="flex justify-between">
+            <div className="flex items-center gap-2">
+              <h3>Faucet DAI</h3>
+              <img src={DAI} alt="dai" className="h-5 w-5" />
+            </div>
+            <div>
+              <img src={Metamask} alt="metamask" className="h-5 w-5 cursor-pointer" />
+            </div>
+          </div>
+        }
+        footer={<Button text="Faucet" type="secondary" />}
+        body={
+          <InputCustom
+            placeHolder="Amount"
+            classNameInput="py-3 px-2"
+            rightAction={<div className="cursor-pointer mr-2">Max</div>}
+          />
+        }
+      />
+      <div
+        className="bg-cover blur-3xl bg-center w-full h-6 absolute top-40 right-28"
+        style={{ backgroundImage: `url(${Mobo})` }}
+      ></div>
+      <div className="faucet-banner mx-auto max-w-7xl py-10 ">
+        <CardWrapper className="w-full p-5" hasShadow={true}>
+          <div className="flex flex-col gap-3 ">
+            <div className="title flex items-center gap-3">
+              <img src={ETH} alt="eth" className="h-10 w-10" />
+              <h1 className="text-2xl">Ethereum Market</h1>
+            </div>
+            <div className="text-zinc-500 text-sm">
+              With testnet Faucet you can get free assets to test the ZK Protocol. Make sure to switch your wallet
+              provider to the appropriate testnet network, select desired asset, and click ‘Faucet’ to get tokens
+              transferred to your wallet. The assets on a testnet are not “real,” meaning they have no monetary value.
+            </div>
+          </div>
+        </CardWrapper>
+      </div>
+      <div className="faucet-list mx-auto max-w-7xl">
+        <h4 className="mb-3">Test Assets</h4>
+        <TableCustom columnDef={columnDef} data={data} cellStyle="py-3" />
+      </div>
+    </div>
+  )
 }
 
 export default FaucetPage
