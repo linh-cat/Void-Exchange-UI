96076d3f64c8d0dc162fd49d34a2eae1322b6dc3
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-05-30 19:30:01 +0700

    FE: make change header connect button

--- src/components/common/ConnectWalletButton.jsx
+++ src/components/common/ConnectWalletButton.jsx
@@ -2,46 +2,24 @@ import React from "react"
 import useUserWindow from "../../hooks/useUserWindow"
 import "./ConnectWalletButton.css"
 import Button from "@components/Button/Button"
-import { useAccount, useNetwork, useConnect, useDisconnect } from "wagmi"
-import { MetaMaskConnector } from "wagmi/connectors/metaMask"
-
+import { useAccount, useNetwork, useConnect, useBalance } from "wagmi"
+const truncate = (string, limit) => {
+  if (string.length <= limit) {
+    return string
+  }
+  return string.slice(0, limit) + "..." + string.slice(string.length - 4, string.length)
+}
 const ConnectWalletButton = ({ imgSrc }) => {
   const { width } = useUserWindow()
   const { connect, connectors, error, isLoading, pendingConnector } = useConnect()
-  const { disconnect } = useDisconnect()
-
   const { address, isConnected } = useAccount()
   const { chain } = useNetwork()
+  const { data: balance } = useBalance({ address })
 
-  const openConnectModal = () => {
-    console.log("chain", chain)
-    console.log("test test")
-    connect()
-  }
-  const openAccountModal = () => {}
   const openChainModal = () => {}
 
   return (
-    // Note: If your app doesn't use authentication, you
-    // can remove all 'authenticationStatus' checks
-    <div
-    // {...{
-    //   "aria-hidden": true,
-    //   style: {
-    //     opacity: 0,
-    //     pointerEvents: "none",
-    //     userSelect: "none"
-    //   }
-    // }}
-    >
-      <Button onClick={() => disconnect()} isDefault="false" className="" text="Disconnect" />
-      {/* <Button */}
-      {/*   onClick={openConnectModal} */}
-      {/*   text={width > 768 ? "Connect Wallet" : "Wallet"} */}
-      {/*   icon={imgSrc && width > 768 && <img className="btn-icon" src={imgSrc} alt="Connect Wallet" />} */}
-      {/*   isDefault={false} */}
-      {/*   className="border px-3 py-1 flex" */}
-      {/* /> */}
+    <div>
       {(() => {
         if (!isConnected) {
           return (
@@ -66,27 +44,15 @@ const ConnectWalletButton = ({ imgSrc }) => {
         return (
           <div className="flex gap-3">
             <>
-              <button onClick={openChainModal} className="flex items-center" type="button">
-                {chain.hasIcon && (
-                  <div
-                    style={{
-                      background: chain.iconBackground
-                    }}
-                    className="w-3 overflow-hidden mr-4 rounded"
-                  >
-                    {chain.iconUrl && (
-                      <img alt={chain.name ?? "Chain icon"} src={chain.iconUrl} style={{ width: 12, height: 12 }} />
-                    )}
+              <Button
+                text={
+                  <div>
+                    {truncate(address, 5)} {chain?.name} {balance?.formatted} {balance?.symbol}
                   </div>
-                )}
-
-                {width > 432 && <div>{chain.name}</div>}
-              </button>
-
-              <button onClick={openAccountModal} type="button" className="flex items-center">
-                {/* {width > 432 && <div className="text-xs">{account.displayName}</div>} */}
-                {/* <div className="text-xs">{account.displayBalance ? ` (${account.displayBalance})` : ""}</div> */}
-              </button>
+                }
+                className="inline-block border px-2 py-3"
+                isDefault={false}
+              />
             </>
           </div>
         )
