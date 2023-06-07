3c8a56a25d836a9969201fdc857694dfa0253f70
anhthii <nguyendanganhthi247@gmail.com>
2023-06-07 07:17:37 +0700

    Fix bug chain not defined

--- src/lib/chains.js
+++ src/lib/chains.js
@@ -3,6 +3,10 @@ import { Constants } from "@void-0x/void-sdk"
 const { ChainId } = Constants
 
 export const isChainSupported = (chain) => {
+  if (!chain) {
+    return false
+  }
+
   const chainIds = [ChainId.Sepolia, ChainId.Goerli]
   return chainIds.includes(chain.id)
 }
