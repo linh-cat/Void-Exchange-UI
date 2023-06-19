7360ecefbd7bba916e289ba7f62dc7dba299710c
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-06-19 17:40:21 +0700

    FE: responsive footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,40 +5,44 @@ import { Link } from "react-router-dom"
 const Footer = () => {
   return (
     <div className="py-10 mt-10 max-w-7xl mx-auto container flex flex-col gap-5">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5">
-        <div>
-          <img src={VoidIcon} alt="void icon" className="w-10 h-10" />
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
+        <div className="relative">
+          <img
+            src={VoidIcon}
+            alt="void icon"
+            className="w-10 h-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          />
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 text-center lg:text-left">
           <h3>Void Exchange</h3>
           <Link className="text-slate-500 text-sm hover">App</Link>
           <Link className="text-slate-500 text-sm hover">Trading</Link>
           <Link className="text-slate-500 text-sm hover">Documentation</Link>
           <Link className="text-slate-500 text-sm hover">Security</Link>
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 text-center lg:text-left">
           <h3>Void Exchange</h3>
           <Link className="text-slate-500 text-sm hover">App</Link>
           <Link className="text-slate-500 text-sm hover">Trading</Link>
           <Link className="text-slate-500 text-sm hover">Documentation</Link>
           <Link className="text-slate-500 text-sm hover">Security</Link>
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 text-center lg:text-left">
           <h3>Void Exchange</h3>
           <Link className="text-slate-500 text-sm hover">App</Link>
           <Link className="text-slate-500 text-sm hover">Trading</Link>
           <Link className="text-slate-500 text-sm hover">Documentation</Link>
           <Link className="text-slate-500 text-sm hover">Security</Link>
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 text-center lg:text-left">
           <h3>Governance</h3>
-          <Link className="text-slate-500 text-sm hover flex items-center gap-3">
+          <Link className="text-slate-500 text-sm hover flex items-center gap-3 justify-center lg:justify-start">
             <img src={GithubMarkIcon} alt="github" className="w-4 h-4" /> <div>Github</div>
           </Link>
-          <Link className="text-slate-500 text-sm hover flex items-center gap-3">
+          <Link className="text-slate-500 text-sm hover flex items-center gap-3 justify-center lg:justify-start">
             <img src={DiscordIcon} alt="discord" className="w-4 h-4 bg-white rounded" /> <div>Discord</div>
           </Link>
-          <Link className="text-slate-500 text-sm hover flex items-center gap-3">
+          <Link className="text-slate-500 text-sm hover flex items-center gap-3 justify-center lg:justify-start">
             <img src={TwitterIcon} alt="twitter" className="w-4 h-4 bg-white rounded" /> <div>Twitter</div>
           </Link>
         </div>
