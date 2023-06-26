ca2cc2ae69d279d4665d2234c2c1d87f5205a209
anhthii <nguyendanganhthi247@gmail.com>
2023-06-26 17:46:36 +0700

    Fix bug buffer is not defined

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client"
 import "./styles/index.css"
 import "rc-slider/assets/index.css"
 import App from "./App"
+import * as buffer from "buffer"
+window.Buffer = buffer.Buffer
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
