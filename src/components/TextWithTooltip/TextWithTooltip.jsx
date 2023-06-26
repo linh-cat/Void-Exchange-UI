43a3f16f09ac1faa59b4c79fb75097665d905ce0
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-06-26 21:46:06 +0700

    FE: add props textwithtooltip

--- src/components/TextWithTooltip/TextWithTooltip.jsx
+++ src/components/TextWithTooltip/TextWithTooltip.jsx
@@ -3,10 +3,10 @@ import cx from "classnames"
 
 import "./TextWithTooltip.css"
 
-const TextWithTooltip = ({ tooltip, text, classNameTooltip }) => {
+const TextWithTooltip = ({ tooltip, text, classNameTooltip, className }) => {
   return (
     <div className={cx("relative text-with-tooltip")}>
-      <h5 className="dotted-underline text-slate-500 cursor-pointer">{text}</h5>
+      <h5 className={cx("dotted-underline text-slate-500 cursor-pointer", className)}>{text}</h5>
       <div className={cx("tooltip card absolute left-0 shadow px-2 py-3 rounded", classNameTooltip)}>{tooltip}</div>
     </div>
   )
