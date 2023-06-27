99250abcc3280f25d8016c0ebc25a7da1c4e2878
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-06-27 14:07:35 +0700

    FE: fix border spiner

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,7 +1,7 @@
 const Spinner = () => {
   return (
     <div
-      className="inline-block h-5 w-5 animate-spin rounded-full border-4 border-solid border-current
+      className="inline-block h-5 w-5 animate-spin rounded-full border-2 border-solid border-current
         border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
       role="status"
     >
