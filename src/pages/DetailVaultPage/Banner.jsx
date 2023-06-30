c3eec0a1fcc1f39915ea417e1fa9f34f9eab8272
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-06-30 22:21:29 +0700

    FE: change style rewards

--- src/pages/DetailVaultPage/Banner.jsx
+++ src/pages/DetailVaultPage/Banner.jsx
@@ -3,58 +3,45 @@ import cx from "classnames"
 const Banner = ({ id, bg, currentVault, capacity, icon, title }) => {
   return (
     <div className={cx({ banner: true }, bg)}>
-      <div className="container mx-auto max-w-7xl flex flex-col md:flex-row gap-5 justify-between items-center">
-        <div className="flex flex-col gap-5 col-span-2 w-96">
-          <h1 className="font-medium text-3xl">{title}</h1>
-          <div className="capacity flex flex-col">
-            <div className="current-deposit flex justify-between">
-              <h3 className="text-slate-500">Current Vault Deposits</h3>
-              <div>{currentVault}</div>
+      <div className="container mx-auto max-w-7xl grid grid-cols-1 xl:grid-cols-4 gap-5 justify-between items-center">
+        <div className="lg:col-span-3">
+          <h1 className="font-medium text-3xl mb-3">{title}</h1>
+          <div className="flex flex-col xl:flex-row items-center gap-10">
+            <div className="capacity flex flex-col w-1/2 xl:w-1/3">
+              <div className="current-deposit flex justify-between">
+                <h3 className="text-slate-500">Current Vault Deposits</h3>
+                <div>{currentVault}</div>
+              </div>
+              <div className="rank relative mt-3 mb-5">
+                <div className="current absolute top-0 w-1/2 h-2 bg-white z-10"></div>
+                <div className="max absolute top-0 w-full h-2 bg-white"></div>
+              </div>
+              <div className="max-vault flex justify-between">
+                <h3 className="text-slate-500">Max Vault Capacity</h3>
+                <div>{capacity}</div>
+              </div>
             </div>
-            <div className="rank relative mt-3 mb-5">
-              <div className="current absolute top-0 w-1/2 h-2 bg-white z-10"></div>
-              <div className="max absolute top-0 w-full h-2 bg-white"></div>
-            </div>
-            <div className="max-vault flex justify-between">
-              <h3 className="text-slate-500">Max Vault Capacity</h3>
-              <div>{capacity}</div>
-            </div>
-          </div>
-          <div
-            className={cx("flex items-center justify-between border rounded p-2 shadow", {
-              "eth-border": id === 1,
-              "btc-border": id === 2,
-              "usdc-border": id === 4
-            })}
-          >
-            <div>
-              <h3
-                className={cx("text-slate-500", {
-                  "eth-color": id === 1,
-                  "btc-color": id === 2,
-                  "usdc-color": id === 4
-                })}
-              >
-                Rewards
-              </h3>
-              <div className="text-sm">2 ETH</div>
-            </div>
-            <div>
-              <h3
-                className={cx("text-slate-500", {
-                  "eth-color": id === 1,
-                  "btc-color": id === 2,
-                  "usdc-color": id === 4
-                })}
-              >
-                ENDS
-              </h3>
-              <div className="text-sm">17D 15H 13M</div>
+            {/* right */}
+            <div
+              className={cx("w-1/2 xl:w-1/3 flex items-center justify-between rounded px-2 py-3", {
+                "eth-linear": id === 1,
+                "btc-linear": id === 2,
+                "usdc-border": id === 4
+              })}
+            >
+              <div>
+                <h3 className={cx("text-white", {})}>Rewards</h3>
+                <div className="text-sm">2 ETH</div>
+              </div>
+              <div>
+                <h3 className={cx("text-white", {})}>ENDS</h3>
+                <div className="text-sm">17D 15H 13M</div>
+              </div>
             </div>
           </div>
         </div>
         <div className="relative">
-          <img src={icon} alt="icon" className="w-full h-full" />
+          <img src={icon} alt="icon" className="w-1/2 h-full mx-auto" />
         </div>
       </div>
     </div>
