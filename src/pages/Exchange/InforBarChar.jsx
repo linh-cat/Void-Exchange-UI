bd120d31e3bf564c286a7fa13270be5c16c6cf4d
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-07-01 18:49:04 +0700

    FE: increase gap infor bar

--- src/pages/Exchange/InforBarChar.jsx
+++ src/pages/Exchange/InforBarChar.jsx
@@ -14,18 +14,18 @@ const InforBarChar = () => {
         </div>
       </div>
       <div className="group-infor px-3 py-1 xl:py-0 xl:px-0 flex gap-3 2xl:gap-5 overflow-x-auto no-scrollbar">
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
           <h3 className="text-xs text-slate-500">Mark</h3>
           <div className="text-xs mt-auto">28,650.00</div>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
           <h3 className="text-xs text-slate-500">Index</h3>
           <div className="text-xs mt-auto">28,650.00</div>
         </div>
         <div className="flex justify-center items-center">
           <div className="h-full w-1px bg-slate-700"></div>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
           <h3 className="text-xs text-slate-500">24h Change(%)</h3>
           <div className="text-xs mt-auto flex items-center gap-1 green-up">
             <span> 0.34% </span>
@@ -35,46 +35,46 @@ const InforBarChar = () => {
         <div className="flex justify-center items-center">
           <div className="h-full w-1px bg-slate-700"></div>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
           <h3 className="text-xs text-slate-500">24h Change</h3>
           <div className="text-xs mt-auto green-up">163.72</div>
         </div>
         <div className="flex justify-center items-center">
           <div className="h-full w-1px bg-slate-700"></div>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
           <h3 className="text-xs text-slate-500">24h High</h3>
           <div className="text-xs mt-auto">28,888.32</div>
         </div>
         <div className="flex justify-center items-center">
           <div className="h-full w-1px bg-slate-700"></div>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
           <h3 className="text-xs text-slate-500">24h Low</h3>
           <div className="text-xs mt-auto">27,033.61</div>
         </div>
         <div className="flex justify-center items-center">
           <div className="h-full w-1px bg-slate-700"></div>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
           <h3 className="text-xs text-slate-500">Volume</h3>
           <div className="text-xs mt-auto">1,505.660</div>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
           <h3 className="text-xs text-slate-500">Volume(BTC)</h3>
           <div className="text-xs mt-auto">55.003</div>
         </div>
         <div className="flex justify-center items-center">
           <div className="h-full w-1px bg-slate-700"></div>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
           <h3 className="text-xs text-slate-500">Next Fund Rate</h3>
           <div className="text-xs mt-auto red-down">-0.00013%</div>
         </div>
         <div className="flex justify-center items-center">
           <div className="h-full w-1px bg-slate-700"></div>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
           <h3 className="text-xs text-slate-500">Countdown</h3>
           <div className="text-xs mt-auto">00:46:00</div>
         </div>
