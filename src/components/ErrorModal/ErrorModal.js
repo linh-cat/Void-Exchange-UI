43cc4df637309af98bf15391e08a16a3265116b8
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-07-14 11:30:29 +0700

    change style modal error

--- src/components/ErrorModal/ErrorModal.js
+++ src/components/ErrorModal/ErrorModal.js
@@ -10,11 +10,11 @@ const ErrorModal = ({ title = 'Transaction Error', shortMessage = 'User denied t
     }, [])
 
     return (
-        <div className={cx("card shadow border fixed w-96 rounded p-5 flex flex-col gap-2 text-sm transition-opacity z-50 top-20 right-5", { visible: isVisible, "fade-in": true, })}>
+        <div className={cx("bg-slate-800 shadow border fixed w-96 rounded p-5 flex flex-col gap-2 text-sm transition-opacity z-50 top-20 right-5", { visible: isVisible, "fade-in": true, })}>
             <img src={CancelDefault} alt='cancel' className='w-5 h-5 absolute right-3 top-3 cursor-pointer' onClick={() => setIsVisible(false)} />
-            <h3 className='text-title-section text-base'>{title}</h3>
-            <p className='text-slate-500 '>{shortMessage}</p>
-            <p className='text-error break-words'>{contentMessage}</p>
+            <h3 className='text-error text-base'>{title}</h3>
+            <p className='text-slate-400'>{shortMessage}</p>
+            <p className='break-words text-slate-300 bg-slate-950 rounded p-3'>{contentMessage}</p>
         </div>
     )
 }
