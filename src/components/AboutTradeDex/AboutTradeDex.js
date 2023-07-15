3a1216d82c6cb3faf2b0c25ff91da2e98243933e
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2023-07-16 00:01:06 +0700

    change style title homepage

--- src/components/AboutTradeDex/AboutTradeDex.js
+++ src/components/AboutTradeDex/AboutTradeDex.js
@@ -10,9 +10,9 @@ const AboutTradeDex = () => {
                 <p className="text-sm">We are continuously launching new Perpetual Contract markets.</p>
             </div>
             <section className="grid grid-cols-1 lg:grid-cols-2 gap-10">
-                <div className="">
-                    <h1 className="text-2xl font-bold">Seamless trading experience with a slick UI</h1>
-                    <p className="mt-5 text-slate-500 break-all">
+                <div className="w-11/12">
+                    <h1 className="text-5xl font-bold break-words">Seamless trading experience with a slick UI</h1>
+                    <p className="mt-5 text-slate-500 break-words leading-7">
                         We focus on UI/UX and aim to provide our users with the best trading experience. We worked on little details and will always be listening to user's feedbacks and keep iterating the UI/UX of the platform.
                     </p>
                 </div>
@@ -27,19 +27,19 @@ const AboutTradeDex = () => {
             </section>
 
             <section className='grid grid-cols-1 lg:grid-cols-2 gap-10'>
-                <div className='overflow-hidden border shadow rounded '>
+                <div className='overflow-hidden border shadow rounded w-11/12'>
                     <img src={VaultBG} alt='vault-bg' className='object-cover' />
                 </div>
-                <div>
-                    <h2 className='text-2xl font-bold'>Earn trading fees from Liquidity Vaults</h2>
-                    <p className='mt-5 text-slate-500 break-all'>Maximize your earnings by depositing your liquidity tokens into our Liquidity Vaults. Risk is isolated for each vault. Start earning confidently.</p>
+                <div className="">
+                    <h2 className='text-5xl font-bold break-words'>Earn trading fees from Liquidity Vaults</h2>
+                    <p className='mt-5 text-slate-500 break-words w-10/12 leading-7'>Maximize your earnings by depositing your liquidity tokens into our Liquidity Vaults. Risk is isolated for each vault. Start earning confidently.</p>
                 </div>
             </section>
 
             <section className='flex flex-col gap-5'>
                 <div className='w-1/2 mx-auto text-center'>
-                    <h2 className='text-2xl font-bold'>Self-governed perpetual market</h2>
-                    <p className='text-slate-500 break-all mt-5'>Launch and profit from your own perpetual market with simple clicks. You can launch a perpetual market of any tokens, from crypto currencies to real-world assets and even meme coins.</p>
+                    <h2 className='text-5xl font-bold break-words'>Self-governed perpetual market</h2>
+                    <p className='text-slate-500 break-words mt-5'>Launch and profit from your own perpetual market with simple clicks. You can launch a perpetual market of any tokens, from crypto currencies to real-world assets and even meme coins.</p>
                 </div>
                 <div className='overflow-hidden border shadow rounded w-1/2 mx-auto'>
                     <img src={SelfGovernedMarket} alt='self-bg' className='object-cover w-full h-full' />
